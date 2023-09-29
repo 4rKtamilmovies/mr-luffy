11962d1e3e852992a360cd50581dbafe849330a1
4rKtamilmovies <109856920+4rKtamilmovies@users.noreply.github.com>
2023-09-29 14:04:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -72,7 +72,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>ʜᴇʏ ᴅᴜᴅᴇ 😍 ,\n\nʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇs ꜰʀᴏᴍ ʜᴇʀᴇ. ʀᴇǫᴜᴇsᴛ ᴏɴ ᴏᴜʀ <a href=https://t.me/bollywoodsouthhindimovieswebse>ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ</a>.</b>")
+    await message.reply_text("<b>ʜᴇʏ ᴅᴜᴅᴇ 😍 ,\n\nʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇs ꜰʀᴏᴍ ʜᴇʀᴇ. ʀᴇǫᴜᴇsᴛ ᴏɴ ᴏᴜʀ <a href=https://t.me/+3oEtjF8Th585YmZl>ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ</a>.</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
@@ -730,7 +730,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                                 InlineKeyboardButton('💡 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('🔔 ᴜᴘᴅᴀᴛᴇs', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("🧑‍🎓 ᴏᴡɴᴇʀ", url="t.me/rudra_rout")
+                                InlineKeyboardButton("🧑‍🎓 ᴏᴡɴᴇʀ", url="t.me/RRRB23")
                                 ]
                                 ]
                             )
@@ -803,7 +803,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                   InlineKeyboardButton('💡 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url=GRP_LNK),
                   InlineKeyboardButton('🔔 ᴜᴘᴅᴀᴛᴇs', url=CHNL_LNK)
                ],[
-                  InlineKeyboardButton("🧑‍🎓 ᴏᴡɴᴇʀ", url="t.me/rudra_rout")
+                  InlineKeyboardButton("🧑‍🎓 ᴏᴡɴᴇʀ", url="https://t.me/RRRB23")
                  ]
                 ]
             )
